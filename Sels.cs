cd071db49030287619d060f39e687541e134069e
Nikita <draco1997pst@gmail.com>
2024-03-02 12:28:52 +0200

    Добавьте файлы проекта.

---
+++ Sels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Идентификатор продажи
+//Идентификатор продукта
+//Количество проданных единиц
+//Дата продажи
+
+namespace linq
+{
+    record class Sell(int id, int productId, int count, string DataSell);
+    
+}
